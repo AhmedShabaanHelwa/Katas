c84439dc71220178458eeb2909ac96118a77dcbc
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-20 18:30:17 +0300

    Refactoring - The Parrot Kata - 7 - Override the method in AfricanParrot to replace the original getSpeed() and push the needed members down.

--- Refactoring/src/main/java/parrot/AfricanParrot.java
+++ Refactoring/src/main/java/parrot/AfricanParrot.java
@@ -1,12 +1,25 @@
 package parrot;
 
 public class AfricanParrot extends Parrot {
+    private static final double LOAD_FACTOR = 9.0;
+    private final int numberOfCoconuts;
+
     public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         super(numberOfCoconuts, voltage, isNailed);
+        this.numberOfCoconuts = numberOfCoconuts;
     }
 
     @Override
     public ParrotTypeEnum getType() {
         return ParrotTypeEnum.AFRICAN;
+    }
+
+    @Override
+    public double getSpeed() {
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+    }
+
+    private double getLoadFactor() {
+        return LOAD_FACTOR;
     }
 }
