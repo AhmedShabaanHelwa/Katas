e6bcf350dd0df77c6b32a199d39b58575e6c3934
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-20 18:31:03 +0300

    Refactoring - The Parrot Kata - 7 - Replace conditional by polymorphism - 7.2 Remove African case in base getSpeed() due to polymorphism replacement.

--- Refactoring/src/main/java/parrot/Parrot.java
+++ Refactoring/src/main/java/parrot/Parrot.java
@@ -3,21 +3,17 @@ package parrot;
 public abstract class Parrot {
 
     private static final double BASE_SPEED = 12.0;
-    private static final double LOAD_FACTOR = 9.0;
     private static final double MAX_BASE_SPEED = 24.0;
-    private final int numberOfCoconuts;
     private final double voltage;
     private final boolean isNailed;
 
     public Parrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        this.numberOfCoconuts = numberOfCoconuts;
         this.voltage = voltage;
         this.isNailed = isNailed;
     }
 
     public double getSpeed() {
         return switch (getType()) {
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
             case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
             default -> throw new IllegalArgumentException("Invalid type of Parrot");
         };
@@ -27,10 +23,6 @@ public abstract class Parrot {
 
     private double getBaseSpeed(double voltage) {
         return Math.min(MAX_BASE_SPEED, voltage * getBaseSpeed());
-    }
-
-    private double getLoadFactor() {
-        return LOAD_FACTOR;
     }
 
     protected double getBaseSpeed() {
