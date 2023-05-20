e7de8202766ae4a5cc3644c8cc4a2871605371b6
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-20 15:47:13 +0300

    Refactoring - The Parrot Kata - 4. Extract subclasses - 4.1 Extract EuropeanParrot and add it to the ParrotFactory.

---
+++ Refactoring/src/main/java/parrot/EruopeanParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class EruopeanParrot extends Parrot {
+    public EruopeanParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(type, numberOfCoconuts, voltage, isNailed);
+    }
+}
--- Refactoring/src/main/java/parrot/ParrotFactory.java
+++ Refactoring/src/main/java/parrot/ParrotFactory.java
@@ -7,6 +7,7 @@ public class ParrotFactory {
     }
 
     public static Parrot create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        if (ParrotTypeEnum.EUROPEAN.equals(type)) return new EruopeanParrot(type, numberOfCoconuts, voltage, isNailed);
         return new Parrot(type, numberOfCoconuts, voltage, isNailed);
     }
 }
