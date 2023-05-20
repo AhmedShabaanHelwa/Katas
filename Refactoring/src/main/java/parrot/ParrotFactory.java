abba8174bf0d268dc55c6a8398d379996dcab9de
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-20 15:53:26 +0300

    Refactoring - The Parrot Kata - 4. Extract subclasses - 4.3 Extract NorwegianBlueParrot and add it to the ParrotFactory.

--- Refactoring/src/main/java/parrot/ParrotFactory.java
+++ Refactoring/src/main/java/parrot/ParrotFactory.java
@@ -8,6 +8,10 @@ public class ParrotFactory {
 
     public static Parrot create(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         if (ParrotTypeEnum.EUROPEAN.equals(type)) return new EruopeanParrot(type, numberOfCoconuts, voltage, isNailed);
-        return new Parrot(type, numberOfCoconuts, voltage, isNailed);
+        else if (ParrotTypeEnum.AFRICAN.equals(type))
+            return new AfricanParrot(type, numberOfCoconuts, voltage, isNailed);
+        else if (ParrotTypeEnum.NORWEGIAN_BLUE.equals(type))
+            return new NorwegianBlueParrot(type, numberOfCoconuts, voltage, isNailed);
+        else throw new IllegalArgumentException("Unknown type of parrots");
     }
 }
