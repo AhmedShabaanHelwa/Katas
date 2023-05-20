e028657708ddbcbb99eca5cb9bb1c41f523a3537
Ahmed Shaban <ahmed_shaban@mentor.com>
2023-05-20 18:42:59 +0300

    Refactoring - The Parrot Kata - 8.1 Override the method in NorwegianBlueParrot to replace the original getSpeed() and push the needed members down.

--- Refactoring/src/main/java/parrot/NorwegianBlueParrot.java
+++ Refactoring/src/main/java/parrot/NorwegianBlueParrot.java
@@ -1,12 +1,24 @@
 package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
+    private static final double MAX_BASE_SPEED = 24.0;
+    private final boolean isNailed;
+    private final double voltage;
+
     public NorwegianBlueParrot(double voltage, boolean isNailed) {
         super(voltage, isNailed);
+        this.voltage = voltage;
+        this.isNailed = isNailed;
     }
 
     @Override
     public ParrotTypeEnum getType() {
         return ParrotTypeEnum.NORWEGIAN_BLUE;
     }
+
+    @Override
+    public double getSpeed() {
+        return isNailed ? 0 : Math.min(MAX_BASE_SPEED, voltage * getBaseSpeed());
+    }
+
 }
--- Refactoring/src/main/java/parrot/Parrot.java
+++ Refactoring/src/main/java/parrot/Parrot.java
@@ -3,9 +3,6 @@ package parrot;
 public abstract class Parrot {
 
     private static final double BASE_SPEED = 12.0;
-    private static final double MAX_BASE_SPEED = 24.0;
-    private final double voltage;
-    private final boolean isNailed;
 
     public Parrot(double voltage, boolean isNailed) {
         this.voltage = voltage;
@@ -20,10 +17,6 @@ public abstract class Parrot {
     }
 
     public abstract ParrotTypeEnum getType();
-
-    private double getBaseSpeed(double voltage) {
-        return Math.min(MAX_BASE_SPEED, voltage * getBaseSpeed());
-    }
 
     protected double getBaseSpeed() {
         return BASE_SPEED;
